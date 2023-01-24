04f78ad79e2a96b7867fc7b35fb7ee120f749876
MauCoden95 <mauro.mig22@hotmail.com>
2023-01-24 16:41:44 -0300

    Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,42 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
+    <!--Link Estilos CSS-->
+    <link rel="stylesheet" href="http://localhost/pin-chat/resources/css/Styles.css">
+
+    <!--Font Awesome-->
+    <script src="https://kit.fontawesome.com/7483adbd94.js" crossorigin="anonymous"></script>
+
+    <title>Pin-chat</title>
+</head>
+<body>
+    <header id="header">
+    <img src="http://localhost/pin-chat/resources/assets/img/Logo.png" id="logo" alt="logo" style="width: 20rem;">
+
+        <h1>Bienvenido, {{ Auth::user()->username }}</h1>
+
+        <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <a aria-current="page" href="route('logout')" onclick="event.preventDefault();
+                    this.closest('form').submit();">Cerrar sesión <i class="fas fa-sign-out-alt"></i></a>
+        </form>
+    <!-- </header><i class="fas fa-paper-plane"></i> -->
+    </header>
+
+
+    <section id="screen">
+        <div>
+            <input type="text" name="name" class="ipt_message" autocomplete="off"/>
+            <a href=""><i class="fas fa-paper-plane"></i></a>
         </div>
-    </div>
-</x-app-layout>
+        
+    </section>
+
+
+</body>
+</html>
